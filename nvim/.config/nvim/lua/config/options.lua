5f59310309aa7a371e814840ff6fa3f23a4fa2a4
jsencinas <jesanchezencinas@gmail.com>
2025-12-02 20:24:15 -0600

    Changed nvim file navigation sorting

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -12,6 +12,13 @@ vim.opt.scrolloff = 8
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.g.netrw_liststyle = 3
+-- alphabetical sorting
+vim.g.netrw_sort_by = 'name'
+vim.g.netrw_sort_direction = 'normal'
+-- directories first
+vim.g.netrw_sort_sequence = '[\\/],*'
+
 vim.cmd([[
     highlight diagnosticunderlineerror gui=undercurl guisp=red
     highlight diagnosticunderlinewarn  gui=undercurl guisp=yellow
