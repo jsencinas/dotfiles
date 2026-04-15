c9503bd7c03c03a6c597df0d4cbe1c91b2c41cb1
jsencinas <jesanchezencinas@gmail.com>
2026-04-14 22:14:11 -0600

    Updated gnome stuff and readme

--- gnome/updateGnomeConfig.sh
+++ gnome/updateGnomeConfig.sh
@@ -3,12 +3,11 @@
 DOTFILES=~/dotfiles
 
 # Keybindings
-dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.ini
-
+dconf dump /org/gnome/desktop/wm/keybindings/ > $DOTFILES/gnome/keybindings.ini
 # Extensions names list
 gnome-extensions list > $DOTFILES/gnome/extensions.txt
 
 # Extensions settings
 dconf dump /org/gnome/shell/extensions/ > $DOTFILES/gnome/extensions-settings.ini
 
-echo "Finished udpating GNOME settings. Git actions pending (add, commit and push)"
+echo "Finished updating GNOME settings. Git actions pending (add, commit and push)"
